9408620916418f7492f6f23ffcab05c15828434f
trim1978 <slider78@rambler.ru>
2022-01-14 03:10:58 +0500

    almost done

---
+++ src/test/java/ru/otus/trim/boot/TrimDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.trim.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrimDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
